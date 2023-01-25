f1474fb6b54bd30e64dee1a37177672b31f04960
Teodor-Ioan Gavrilescu <teodor.gavrilescu@ibm.com>
2023-01-25 13:11:20 +0200

    Webpack Full Setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useQuery, gql } from '@apollo/client'
+
+const DATA = gql`
+  query ExampleQuery {
+    allPlanets {
+      planets {
+        name
+      }
+    }
+  }
+`
+
+export const App = () => {
+  const { loading, error, data } = useQuery(DATA)
+
+  if (loading) return <>Loading...</>
+
+  if (error) return <>Error! ${error.message}</>
+
+  return (
+    <>
+      <div>
+        <h1>Hello Teo!</h1>
+      </div>
+      {JSON.stringify(data)}
+    </>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
